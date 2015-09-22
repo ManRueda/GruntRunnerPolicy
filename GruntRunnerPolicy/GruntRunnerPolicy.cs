068fadf4f8cedb69322125efe5082f75e00ed428
Manuel Rueda <manuel.rueda.un@gmail.com>
2015-09-22 00:10:04 -0300

    Fix some errors

--- GruntRunnerPolicy/GruntRunnerPolicy.cs
+++ GruntRunnerPolicy/GruntRunnerPolicy.cs
@@ -7,58 +7,45 @@ using System.IO;
 using System.Diagnostics;
 
 namespace GruntRunnerPolicy {
+
 	[Serializable]
-	class GruntRunnerPolicy: PolicyBase {
+	public class GruntRunnerPolicy: PolicyBase {
         GruntRunner runner;
 
+        //Description of the policy
 		public override string Description {
 			get {
 				return@"This policy runs a custom Grunt Task. If the task fails, the check-in is rejected.";
 			}
 		}
 
-		// This is a string that is stored with the policy definition on the source
-		// control server. If a user does not have the policy plug-in installed, this string
-		// is displayed.  You can use this to explain to the user how they should 
-		// install the policy plug-in.
+        //Text to display to the user when he doesn't have the policy in his system.
 		public override string InstallationInstructions {
 			get {
-
 				return@"Instructions abount how to get the policy.";
 			}
 		}
 
-		// This string identifies the type of policy. It is displayed in the 
-		// policy list when you add a new policy to a Team Project.
+        //Type of policy, this string will be displayed in the policies combo
 		public override string Type {
 			get {
 				return "Grunt validations";
 			}
 		}
 
-		// This string is a description of the type of policy. It is displayed 
-		// when you select the policy in the Add Check-in Policy dialog box.
+        //Description of the type of policy
 		public override string TypeDescription {
 			get {
                 return @"This policy runs a custom Grunt Task. If the task fails, the check-in is rejected.";
 			}
 		}
 
-		// This method is called by the policy framework when you create 
-		// a new check-in policy or edit an existing check-in policy.
-		// You can use this to display a UI specific to this policy type 
-		// allowing the user to change the parameters of the policy.
 		public override bool Edit(IPolicyEditArgs args) {
 			// Do not need any custom configuration
 			return true;
 		}
 
-		// This method performs the actual policy evaluation. 
-		// It is called by the policy framework at various points in time
-		// when policy should be evaluated. In this example, the method 
-		// is invoked when various asyc events occur that may have 
-		// invalidated the current list of failures.
-
+        //This method perfomance the check-in evaluation
 		public override PolicyFailure[] Evaluate() {
 
 			var configs = new List<string>();
@@ -97,15 +84,12 @@ namespace GruntRunnerPolicy {
 			}
 		}
 
-		// This method is called if the user double-clicks on 
-		// a policy failure in the UI. In this case a message telling the user 
-		// to supply some comments is displayed.
+        //This method is executed when the user double-clicks the UI of the policy
 		public override void Activate(PolicyFailure failure) {
 			MessageBox.Show("Run the grunt configurated task to see the errors in your code.", "How to fix your policy failure");
 		}
 
-		// This method is called if the user presses F1 when a policy failure 
-		// is active in the UI. In this example, a message box is displayed.
+        //This method is executed when the user ask for the policy help (F1)
 		public override void DisplayHelp(PolicyFailure failure) {
             if (runner != null)
             {
